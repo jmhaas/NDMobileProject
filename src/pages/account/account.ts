b02d4a0f5c861b03dbb2a10e6206f51ce6d4de8c
Stephen Pegram <stephenpegram@Stephens-MacBook-Pro.local>
2017-06-11 11:31:45 -0400

    fixed some bugs in last push

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CampaignPage, Campaign } from '../campaign/campaign';
 
 /**
  * Generated class for the AccountPage page.
