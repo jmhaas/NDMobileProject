dec6b19ff062c365a84baba01cf25323043e5d42
wongandrewp <wong.andrew.p@gmail.com>
2017-06-13 16:13:30 -0400

    campaign looking aestetic

--- src/pages/campaign/campaign.ts
+++ src/pages/campaign/campaign.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { HttpProvider } from '../../providers/http/http';
 
 @Component({
   selector: 'campaign-page',
-  templateUrl: 'campaign.html'
+  templateUrl: 'campaign.html',
+  providers: [ HttpProvider ]
 })
 export class CampaignPage {
   selectedItem: any;
-
-constructor(public navCtrl: NavController, public navParams: NavParams) {
+  profiles: Array<{profile:any}>;
+  attending: Array<{aviPath: string, name: string}>;
+constructor(public navCtrl: NavController, public navParams: NavParams, private httpProvider: HttpProvider) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
+    this.httpProvider.getProfilesJsonData().subscribe((data) => {
+      console.log("What is in the data: ", data);
+      this.profiles = data;
+      this.attending = [];
+      for (let i = 0; i < this.profiles.length; i += 1) {
+        this.attending.push({
+        aviPath: this.profiles[i]["photo"],
+        name: this.profiles[i]["name"],
+      })
+      console.log(this.attending[i]["name"] + this.attending[i]["aviPath"]);
+    }
+    },
+    err => {
+      console.log("Error, "+err);
+    },
+    () => {
+      console.log("Completed");
+    })
   }
 
 }
