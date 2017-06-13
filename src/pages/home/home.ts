f3f7acee0b4dd5f3fa54a211ef2b03d522153a07
Daniel Prahl <daniel.prahl@gmail.com>
2017-06-13 16:21:44 -0400

    "committing so i can pull down"

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
 import { DonatePage } from '../donate/donate';
 import { VolunteerPage } from '../volunteer/volunteer';
 import { OrganizePage } from '../organize/organize';
+import { CampaignsPage } from '../campaigns/campaigns';
+import { AccountPage } from '../account/account';
+import { CommunityPage } from '../community/community';
 
 @Component({
   selector: 'page-home',
@@ -24,6 +28,18 @@ export class HomePage {
 
   goToOrganize() {
     this.navCtrl.push(OrganizePage);
+  }
+
+  goToCampaigns() {
+    this.navCtrl.push(CampaignsPage);
+  }
+
+  goToAccount() {
+    this.navCtrl.push(AccountPage);
+  }
+
+  goToCommunity() {
+    this.navCtrl.push(CommunityPage);
   }
 
 }
